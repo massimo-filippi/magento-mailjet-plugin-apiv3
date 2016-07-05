765e781617fe60cd8ec49aea6466857e49963056
FerhanGit <fismailov@mailjet.com>
2016-07-05 15:46:25 +0300

    Plugin improvements - adding support of queued Email sending

--- plugin/app/code/local/MJ/Customsmtp/Model/Observer.php
+++ plugin/app/code/local/MJ/Customsmtp/Model/Observer.php
@@ -5,87 +5,77 @@
  */
 class MJ_Customsmtp_Model_Observer
 {
-    protected static $fields = array ();
 
-    public function sendTestMail ($observer)
+    protected static $fields = array();
+
+    public function sendTestMail($observer)
     {
-        $data = $observer->getEvent ()->getData ('data_object')->getData ();
+        $data = $observer->getEvent()->getData('data_object')->getData();
 
-        switch ($data ['field'])
-        {
-            case 'login':
-            {
+        switch ($data ['field']) {
+            case 'login': {
                 self::$fields ['username'] = $data ['value'];
-
                 break;
             }
             case 'password':
             case 'test':
-            case 'test_address':
-            {
-                self::$fields [$data ['field']] = $data ['value'];
-
+            case 'test_address': {
+                self::$fields [$data['field']] = $data['value'];
                 break;
             }
         }
-        
-        if (array_key_exists('test', self::$fields) && 4 == count (self::$fields))
-        {
-            $configs = array (
-                array ('ssl://', 465),
-                array ('tls://', 587),
-                array ('', 587),
-                array ('', 588),
-                array ('tls://', 25),
-                array ('', 25),
-                array ('', 80),
+
+        if (array_key_exists('test', self::$fields) && 4 == count(self::$fields)) {
+            $configs = array(
+                array('ssl://', 465),
+                array('tls://', 587),
+                array('', 587),
+                array('', 588),
+                array('tls://', 25),
+                array('', 25),
+                array('', 80),
             );
 
             $host = Mage::getStoreConfig(MJ_Customsmtp_Helper_Config::XML_PATH_SMTP_HOST);
             $connected = FALSE;
 
-            for ($i = 0; $i < count ($configs); ++$i)
-            {
-                $soc = @ fSockOpen ($configs [$i] [0].$host, $configs [$i] [1], $errno, $errstr, 5);
+            for ($i = 0; $i < count($configs); ++$i) {
+                $soc = @fSockOpen($configs[$i][0] . $host, $configs[$i][1], $errno, $errstr, 5);
 
-                if ($soc)
-                {
-                    fClose ($soc);
-
+                if ($soc) {
+                    fClose($soc);
                     $connected = TRUE;
-
                     break;
                 }
             }
 
-            if ($connected)
-            {
-                if ('ssl://' == $configs [$i] [0])
-                {
-                    Mage::getConfig ()->saveConfig (MJ_Customsmtp_Helper_Config::XML_PATH_SMTP_SSL, 'SSL');
-                }
-                elseif ('tls://' == $configs [$i] [0])
-                {
-                    Mage::getConfig ()->saveConfig (MJ_Customsmtp_Helper_Config::XML_PATH_SMTP_SSL, 'TLS');
-                }
-                else
-                {
-                    Mage::getConfig ()->saveConfig (MJ_Customsmtp_Helper_Config::XML_PATH_SMTP_SSL, '');
+            if ($connected) {
+                if ('ssl://' == $configs[$i][0]) {
+                    Mage::getConfig()->saveConfig(MJ_Customsmtp_Helper_Config::XML_PATH_SMTP_SSL, 'SSL');
+                    Mage::getConfig()->saveConfig('system/smtp/ssl', 'SSL');
+                } elseif ('tls://' == $configs[$i][0]) {
+                    Mage::getConfig()->saveConfig(MJ_Customsmtp_Helper_Config::XML_PATH_SMTP_SSL, 'TLS');
+                    Mage::getConfig()->saveConfig('system/smtp/ssl', 'TLS');
+                } else {
+                    Mage::getConfig()->saveConfig(MJ_Customsmtp_Helper_Config::XML_PATH_SMTP_SSL, '');
+                    Mage::getConfig()->saveConfig('system/smtp/ssl', '');
                 }
 
-                Mage::getConfig ()->saveConfig (MJ_Customsmtp_Helper_Config::XML_PATH_SMTP_PORT, $configs [$i] [1]);
+                Mage::getConfig()->saveConfig(MJ_Customsmtp_Helper_Config::XML_PATH_SMTP_PORT, $configs[$i][1]);
+                Mage::getConfig()->saveConfig('system/smtp/port', $configs[$i][1]);
+                Mage::getConfig()->saveConfig('system/smtp/host', $host);
 
-                Mage::getConfig ()->reinit ();
-                Mage::app ()->reinitStores ();
+                Mage::getConfig()->reinit();
+                Mage::app()->reinitStores();
 
                 $to = self::$fields ['test_address'];
-                $from = Mage::getStoreConfig ('trans_email/ident_general/email');
+                $from = Mage::getStoreConfig('trans_email/ident_general/email');
 
-                $Mail = Mage::getModel ('customsmtp/mail');
+                $Mail = Mage::getModel('customsmtp/mail');
 
-                $Mail->setBody (Mage::helper ('customsmtp')->__ ('Your Mailjet configuration is ok!'));
-                $Mail->setIsPlain (TRUE);
-                $Mail->setSubject (Mage::helper ('customsmtp')->__ ('Your test mail from Mailjet'));
+                $Mail->setBody(Mage::helper('customsmtp')->__('Your Mailjet configuration is ok!'));
+                $Mail->setIsPlain(TRUE);
+                $Mail->setSubject(Mage::helper('customsmtp')->__('Your test mail from Mailjet'));
 
                 $Mail
                     ->setFromName('Mailjet')
@@ -94,16 +84,16 @@ class MJ_Customsmtp_Model_Observer
                     ->setToName($to)
                     ->setToEmail($to);
 
-                $sender = Mage::getModel ('customsmtp/email_template')->load (Mage::getStoreConfig (Mage::app ()->getStore ()->getId ()));
+                $sender = Mage::getModel('customsmtp/email_template')->load(Mage::getStoreConfig(Mage::app()->getStore()->getId()));
 
                 $sender->sendMail($Mail, self::$fields);
-            }
-            else
-            {
-                throw new Exception (sPrintF ('Please contact Mailjet support to sort this out.<br /><br />%d - %s', $errno, $errstr));
+            } else {
+                throw new Exception(sPrintF('Please contact Mailjet support to sort this out.<br /><br />%d - %s',
+                    $errno, $errstr));
             }
         }
     }
+
 }
 
 ?>
